159c6c9c9e0664e5a73369de22dbb983bad0c161
jlortiz0 <jlortiz0@users.noreply.github.com>
2023-04-12 14:38:32 -0700

    Disabled by default

--- src/main/java/dzwdz/vertical_hotbar/client/ModConfig.java
+++ src/main/java/dzwdz/vertical_hotbar/client/ModConfig.java
@@ -5,9 +5,9 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = "vertical_hotbar")
 public class ModConfig implements ConfigData {
-    public boolean enabled = true;
+    public boolean enabled = false;
     public boolean attachToTop = false;
-    public boolean flipBar = false;
+    public boolean flipBar = true;
     public boolean hotbarBorder = true;
     public boolean itemTooltip = true;
 }
